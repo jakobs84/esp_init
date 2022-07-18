080901206ee7b9fd6b8f2508876583b0d20e83a5
jakobs84 <jakobs84@o2.pl>
2022-07-18 21:43:38 +0200

    Dodano Flash size

--- src/main.cpp
+++ src/main.cpp
@@ -44,6 +44,27 @@ void setup() {
 
   // Print ESP Local IP Address
   Serial.println(WiFi.localIP());
+    delay(1000);
+  uint32_t realSize = ESP.getFlashChipRealSize();
+  uint32_t ideSize = ESP.getFlashChipSize();
+  FlashMode_t ideMode = ESP.getFlashChipMode();
+
+  Serial.printf("Flash real id:   %08X\n", ESP.getFlashChipId());
+  Serial.printf("Flash real size: %u bytes\n\n", realSize);
+
+  Serial.printf("Flash ide  size: %u bytes\n", ideSize);
+  Serial.printf("Flash ide speed: %u Hz\n", ESP.getFlashChipSpeed());
+  Serial.printf("Flash ide mode:  %s\n", (ideMode == FM_QIO ? "QIO" : ideMode == FM_QOUT ? "QOUT"
+                                                                    : ideMode == FM_DIO  ? "DIO"
+                                                                    : ideMode == FM_DOUT ? "DOUT"
+                                                                                         : "UNKNOWN"));
+
+  if (ideSize != realSize) {
+    Serial.println("Flash Chip configuration wrong!\n");
+    Serial.println("ideSize != realSize  !!!\n");
+  } else {
+    Serial.println("Flash Chip configuration ok.\n");
+  }
 
 }
 
@@ -57,3 +78,4 @@ void loop() {
   else
       Serial.println("LED OFF");
 }
+
